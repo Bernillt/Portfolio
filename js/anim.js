5417c510a6493fb103e276169870e932a72da6d4
Bernillt <83372550+Bernillt@users.noreply.github.com>
2021-05-28 14:48:56 +0200

    Add files via upload

---
+++ js/anim.js
@@ -0,0 +1,31 @@
+(function () {
+				  var $bar, perc, start, update;
+
+				  $bar = $('.percentage');
+
+				  perc = 0;
+
+				  update = function () {
+				    $bar.css("width", perc + '%');
+				    $bar.attr("perc", Math.floor(perc) + '%');
+				    perc += 0.2;
+				    if (Math.floor(perc) === 5) {
+				      $bar.addClass('active');
+				    }
+				    if (Math.floor(perc) === 95) {
+				      $bar.removeClass('active');
+				    }
+				    if (perc >= 100) {
+				      return perc = 0;
+				    }
+				  };
+
+				  start = function () {
+				    var run;
+				    return run = setInterval(update, 10);
+				  };
+
+				  start();
+
+				}).call(this);
+				//# sourceURL=coffeescript
